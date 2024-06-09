a0159fd1c651cada8e19bdff36b0a411fae58131
SaiOhmSaiePaine <saiohmsaieapaine@my.jcu.edu.au>
2024-06-09 16:36:49 +0800

    random numbers

---
+++ prac_03/randoms.py
@@ -0,0 +1,14 @@
+import random
+
+"""
+on line 1 after running, the output is 17. And the biggest number could be 20, while the smallest is 5.
+
+on line 2 after running, the output is 3. And the biggest number could be 10, while the smallest is 3. 
+line 2 could not produce a 4 because the increment value is 2. 
+
+on line 3 after running, the output is 4.3679100285361665. And the biggest number could be 5.5, while the smallest is 2.5.
+
+"""
+
+random_number = random.randint(1, 100)
+print(random_number)
